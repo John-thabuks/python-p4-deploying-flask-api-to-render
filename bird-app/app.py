7cb4813360869416119fb2218a0e249babe183d5
John-thabuks <thabuksjohn@gmail.com>
2024-03-15 22:05:11 +0300

    error fix try

--- bird-app/app.py
+++ bird-app/app.py
@@ -1,5 +1,3 @@
-# app.py
-
 import os
 
 from flask import Flask, jsonify, make_response
@@ -9,14 +7,13 @@ from flask_restful import Api, Resource
 from models import db, Bird
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
-print(os.environ.get('DATABASE_URI'))
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///default.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 
-# db.init_app(app)
+db.init_app(app)
 
 api = Api(app)
 
