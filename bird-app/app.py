3a5b4061f60b7411f69b29f69bfe10ec6d33b8ce
John-thabuks <thabuksjohn@gmail.com>
2024-03-16 14:18:09 +0300

    changes

--- bird-app/app.py
+++ bird-app/app.py
@@ -7,7 +7,7 @@ from flask_restful import Api, Resource
 from models import db, Bird
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///default.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')   #, 'sqlite:///default.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
